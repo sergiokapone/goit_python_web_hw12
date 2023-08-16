625fbfbd5aac3b7112fba5323f1c1c493e1d62af
sergiokapone <sergiy.ponomarenko@gmail.com>
2023-08-16 13:25:57 +0300

    black

--- tests/test_unit_contacts.py
+++ tests/test_unit_contacts.py
@@ -9,18 +9,19 @@ from schemas import ContactCreate
 
 
 class TestAsync(unittest.IsolatedAsyncioTestCase):
-    
     def setUp(self):
         self.session = AsyncMock(spec=AsyncSession)
         self.user = User(
             id=1, email="testuser@example.com", password="qwer1234", confirmed=True
         )
-        self.contact = Contact(first_name = "John",
-        last_name = "Doe",
-        email = "johndoe@example.com",
-        phone_number =  "123456789",
-        birthday =  "2000-01-01",
-        user_id = self.user.id)
+        self.contact = Contact(
+            first_name="John",
+            last_name="Doe",
+            email="johndoe@example.com",
+            phone_number="123456789",
+            birthday="2000-01-01",
+            user_id=self.user.id,
+        )
 
     async def test_create_contact(self):
         body = self.contact
@@ -36,35 +37,42 @@ class TestAsync(unittest.IsolatedAsyncioTestCase):
         result = await get_all_contacts(self.user, self.session)
         self.assertEqual(result, expected_contacts)
 
-    async def test_update_contact_successful(self): 
-            contact_id = 1
-            contact_create = self.contact
-            existing_contact = Contact(id=contact_id, user_id=self.user.id)
-            
-            session_mock = AsyncMock()
-            session_mock.get.return_value = existing_contact
+    async def test_update_contact_successful(self):
+        contact_id = 1
+        contact_create = self.contact
+        existing_contact = Contact(id=contact_id, user_id=self.user.id)
 
-            updated_contact = await update_contact(contact_id, contact_create, self.user, session_mock)
+        session_mock = AsyncMock()
+        session_mock.get.return_value = existing_contact
 
-            self.assertEqual(updated_contact.first_name, contact_create.first_name)
-            self.assertEqual(updated_contact.last_name, contact_create.last_name)
-            self.assertEqual(updated_contact.email, contact_create.email)
-            self.assertEqual(updated_contact.phone_number, contact_create.phone_number)
-            self.assertEqual(str(updated_contact.birthday), contact_create.birthday)
-            self.assertEqual(updated_contact.additional_data, contact_create.additional_data)
-            session_mock.commit.assert_called_once()
-            session_mock.refresh.assert_called_once_with(existing_contact)
+        updated_contact = await update_contact(
+            contact_id, contact_create, self.user, session_mock
+        )
+
+        self.assertEqual(updated_contact.first_name, contact_create.first_name)
+        self.assertEqual(updated_contact.last_name, contact_create.last_name)
+        self.assertEqual(updated_contact.email, contact_create.email)
+        self.assertEqual(updated_contact.phone_number, contact_create.phone_number)
+        self.assertEqual(str(updated_contact.birthday), contact_create.birthday)
+        self.assertEqual(
+            updated_contact.additional_data, contact_create.additional_data
+        )
+        session_mock.commit.assert_called_once()
+        session_mock.refresh.assert_called_once_with(existing_contact)
 
     async def test_delete_contact_successful(self):
-            contact_id = 1
-            contact = Contact(id=contact_id, user_id=self.user.id)
-            
-            session_mock = AsyncMock()
-            session_mock.get.return_value = contact
+        contact_id = 1
+        contact = Contact(id=contact_id, user_id=self.user.id)
 
-            result = await delete_contact(contact_id, self.user, session_mock)
+        session_mock = AsyncMock()
+        session_mock.get.return_value = contact
 
-            self.assertEqual(result, {"message": "Contact deleted", "contact": contact})
-            session_mock.delete.assert_called_once_with(contact)
-            session_mock.commit.assert_called_once()
+        result = await delete_contact(contact_id, self.user, session_mock)
 
+        self.assertEqual(result, {"message": "Contact deleted", "contact": contact})
+        session_mock.delete.assert_called_once_with(contact)
+        session_mock.commit.assert_called_once()
+
+
+if __name__ == "__main__":
+    unittest.main()
